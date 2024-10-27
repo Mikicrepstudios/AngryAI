cd1090c91113ae1d6837580ba9eb3066b440afae
Mikicrep <mikicrepoffical@gmail.com>
2024-10-27 13:45:35 +0100

    Added quitting window

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,28 @@
 #include "SDL2/SDL.h"
 
 int main() {
+    // Important vars
     bool running = true;
     int fps = 60;
 
+    // SDL stuff
     SDL_Window* window = SDL_CreateWindow("Angry AI", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 800, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Event event = {};
 
     while(running) {
+        while(SDL_PollEvent(&event) != 0) {
+            if(event.type == SDL_QUIT) running = false;
+            if(event.type == SDL_KEYDOWN) {
+                if(event.key.keysym.sym == SDLK_ESCAPE) running = false;
+            }
+        }
+
+        // Clear screen
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
 
+        // Show stuff
         SDL_RenderPresent(renderer);
         SDL_Delay(1000 / fps);
     }
