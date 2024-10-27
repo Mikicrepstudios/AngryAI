f58031db5cf30932528c217f1cc4e7f39db9f5f8
Mikicrep <mikicrepoffical@gmail.com>
2024-10-27 14:58:32 +0100

    Added AIs

--- source/graphics/drawentities.cpp
+++ source/graphics/drawentities.cpp
@@ -3,8 +3,20 @@
 
 namespace draw {
     void DrawEntities(settings::SDL_Settings sdlSettings) {
+        // Player rect
         SDL_Rect playerRect = {25, sdlSettings.height - 125, 100, 100};
 
+        // Enemies
+        SDL_Rect entityTopRect = {sdlSettings.width - 125, 25, 100, 100};
+        SDL_Rect entityMiddleRect = {sdlSettings.width - 125, sdlSettings.height / 2 - 50, 100, 100};
+        SDL_Rect entityBottomRect = {sdlSettings.width - 125, sdlSettings.height - 125, 100, 100};
+
+        // Draw player
         draw::DrawRect(sdlSettings.renderer, playerRect, 255, 255, 255);
+
+        // Draw enemies
+        draw::DrawRect(sdlSettings.renderer, entityTopRect, 0, 212, 85);
+        draw::DrawRect(sdlSettings.renderer, entityMiddleRect, 0, 86, 191);
+        draw::DrawRect(sdlSettings.renderer, entityBottomRect, 89, 23, 255);
     }
 }
