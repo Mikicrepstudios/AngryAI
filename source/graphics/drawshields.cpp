b84df8f60a433c5ed289954989ae0a54e2345616
Mikicrep <mikicrepoffical@gmail.com>
2024-11-11 21:28:58 +0100

    Migrated drawshields.cpp to MF

--- source/graphics/drawshields.cpp
+++ source/graphics/drawshields.cpp
@@ -1,14 +1,13 @@
-#include "SDL2/SDL.h"
+#include "mf/core.h"
+#include "mf/graphics.h"
 
-#include "draw.h"
 #include "rects.h"
-#include "settings.h"
 #include "textures.h"
 
 namespace draw {
-    void DrawShields(settings::SDL_Settings sdlSettings, rects::ShieldRects shieldRects, textures::SpecialsTextures specialsTextures, int shieldedAIOrder[3]) {
-        if(shieldedAIOrder[0] == 1) draw::DrawTextureRect(sdlSettings.renderer, shieldRects.topShield, specialsTextures.gptSpecial);
-        if(shieldedAIOrder[1] == 1) draw::DrawTextureRect(sdlSettings.renderer, shieldRects.middleShield, specialsTextures.gptSpecial);
-        if(shieldedAIOrder[2] == 1) draw::DrawTextureRect(sdlSettings.renderer, shieldRects.bottomShield, specialsTextures.gptSpecial);
+    void DrawShields(core::MF_Window &window, rects::ShieldRects shieldRects, textures::SpecialsTextures specialsTextures, int shieldedAIOrder[3]) {
+        if(shieldedAIOrder[0] == 1) draw::DrawTextureRect(window.renderer, shieldRects.topShield, specialsTextures.gptSpecial);
+        if(shieldedAIOrder[1] == 1) draw::DrawTextureRect(window.renderer, shieldRects.middleShield, specialsTextures.gptSpecial);
+        if(shieldedAIOrder[2] == 1) draw::DrawTextureRect(window.renderer, shieldRects.bottomShield, specialsTextures.gptSpecial);
     }
 }
--- source/headers/draw.h
+++ source/headers/draw.h
@@ -14,7 +14,7 @@ namespace draw {
 	void DrawHPBars(core::MF_Window &window, data::Player player, data::AI AIs[3], rects::EntityRects entityRects);
 	extern void DrawSpecialBars(settings::SDL_Settings &sdlSettings, data::Player player, data::AI AIs[3], rects::EntityRects entityRects);
     void DrawEntities(core::MF_Window &window, data::AI AIs[3], int AIOrder[3], textures::EntityTextures &entityTextures, rects::EntityRects entityRects);
-	extern void DrawShields(settings::SDL_Settings sdlSettings, rects::ShieldRects shieldRects, textures::SpecialsTextures specialsTextures, int shieldedAIOrder[3]);
+	void DrawShields(core::MF_Window &window, rects::ShieldRects shieldRects, textures::SpecialsTextures specialsTextures, int shieldedAIOrder[3]);
 
 	// Visuals
 	void DrawGUI(core::MF_Window &window, int level, int timeM, int timeS);
