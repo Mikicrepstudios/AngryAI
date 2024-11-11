4d7deac71eb61f50a266a3ae126ca6930091f464
Mikicrep <mikicrepoffical@gmail.com>
2024-11-11 21:17:34 +0100

    Migrated drawentities.cpp to MF

--- source/graphics/drawentities.cpp
+++ source/graphics/drawentities.cpp
@@ -1,17 +1,19 @@
 #include "SDL2/SDL.h"
 #include <SDL_image.h>
 
+#include "mf/core.h"
+#include "mf/graphics.h"
+
 #include "data.h"
 #include "draw.h"
 #include "settings.h"
 #include "rects.h"
 #include "textures.h"
 
-retrerwaw r// break please
 namespace draw {
-    void DrawEntities(settings::SDL_Settings sdlSettings, data::AI AIs[3], int AIOrder[3], textures::EntityTextures &entityTextures, rects::EntityRects entityRects) {
+    void DrawEntities(core::MF_Window window, data::AI AIs[3], int AIOrder[3], textures::EntityTextures &entityTextures, rects::EntityRects entityRects) {
         // Draw player
-        draw::DrawTextureRect(sdlSettings.renderer, entityRects.playerRect, entityTextures.playerTexture);
+        draw::DrawTextureRect(window.renderer, entityRects.playerRect, entityTextures.playerTexture);
 
         // Draw enemies
         SDL_Rect curRect = {};
@@ -21,9 +23,9 @@ namespace draw {
             else if(x == 2) curRect = entityRects.bottomEnemyRect;
 
             if(AIs[x].health != 0) {
-                if(AIOrder[x] == 1) draw::DrawTextureRect(sdlSettings.renderer, curRect, entityTextures.gptTexture);
-                else if(AIOrder[x] == 2) draw::DrawTextureRect(sdlSettings.renderer, curRect, entityTextures.copilotTexture);
-                else if(AIOrder[x] == 3) draw::DrawTextureRect(sdlSettings.renderer, curRect, entityTextures.geminiTexture);
+                if(AIOrder[x] == 1) draw::DrawTextureRect(window.renderer, curRect, entityTextures.gptTexture);
+                else if(AIOrder[x] == 2) draw::DrawTextureRect(window.renderer, curRect, entityTextures.copilotTexture);
+                else if(AIOrder[x] == 3) draw::DrawTextureRect(window.renderer, curRect, entityTextures.geminiTexture);
             }
         }
     }
--- source/headers/draw.h
+++ source/headers/draw.h
@@ -2,6 +2,8 @@
 
 #include <SDL_ttf.h>
 
+#include "mf/core.h"
+
 #include "data.h"
 #include "settings.h"
 #include "rects.h"
@@ -11,7 +13,7 @@ namespace draw {
     // Game
 	extern void DrawHPBars(settings::SDL_Settings &sdlSettings, data::Player player, data::AI AIs[3], rects::EntityRects entityRects);
 	extern void DrawSpecialBars(settings::SDL_Settings &sdlSettings, data::Player player, data::AI AIs[3], rects::EntityRects entityRects);
-    extern void DrawEntities(settings::SDL_Settings sdlSettings, data::AI AIs[3], int AIOrder[3], textures::EntityTextures &entityTextures, rects::EntityRects entityRects);
+    void DrawEntities(core::MF_Window window, data::AI AIs[3], int AIOrder[3], textures::EntityTextures &entityTextures, rects::EntityRects entityRects);
 	extern void DrawShields(settings::SDL_Settings sdlSettings, rects::ShieldRects shieldRects, textures::SpecialsTextures specialsTextures, int shieldedAIOrder[3]);
 
 	// Visuals
