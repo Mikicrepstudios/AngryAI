d2f4ca1e2662e600c6fd5f774c929ee1087b073d
Mikicrep <mikicrepoffical@gmail.com>
2024-11-11 21:51:00 +0100

    Migrated damageai.cpp to MF

--- source/logic/damageai.cpp
+++ source/logic/damageai.cpp
@@ -1,6 +1,8 @@
+#include "mf/core.h"
+#include "mf/logic.h"
+
 #include "data.h"
-#include "draw.h"
-#include "logic.h"
+//#include "logic.h"
 #include "rects.h"
 #include "settings.h"
 #include "textures.h"
@@ -9,7 +11,7 @@ namespace logic {
     void damageAI(data::Player &player, data::AI AIs[3], int target, int minDamage, int maxDamage, settings::Bullet &bullet, int shieldedAIOrder[3], int &turn) {
         if(target < 4) {
             bullet.y = 4000;
-            int damage = logic::generateRandomNumber(minDamage, maxDamage);
+            int damage = logic::GenRanNum(minDamage, maxDamage);
 
             AIs[target - 1].health -= damage;
             if(AIs[target - 1].health <= 0) AIs[target - 1].health = 0; // Make number not go negative
@@ -25,14 +27,14 @@ namespace logic {
         }
     }
 
-    void specialDamageAI(settings::SDL_Settings &sdlSettings, data::Player &player, data::AI AIs[3], rects::EntityRects entityRects, bool &isAttackedAI, int &attackedAI) {
+    void specialDamageAI(core::MF_Window &window, data::Player &player, data::AI AIs[3], rects::EntityRects entityRects, bool &isAttackedAI, int &attackedAI) {
         if(player.curCharge == player.maxCharge) {
-            SDL_Rect mouseRect = {sdlSettings.mouseX, sdlSettings.mouseY, 1, 1};
+            SDL_Rect mouseRect = {window.mouseX, window.mouseY, 1, 1};
             SDL_Rect selRect = {};
 
-            if(logic::areRectsTouching(mouseRect, entityRects.topEnemyRect)) {attackedAI = 0;}
-            else if(logic::areRectsTouching(mouseRect, entityRects.middleEnemyRect)) {attackedAI = 1;}
-            else if(logic::areRectsTouching(mouseRect, entityRects.bottomEnemyRect)) {attackedAI = 2;}
+            if(logic::AreRectsTouching(mouseRect, entityRects.topEnemyRect)) {attackedAI = 0;}
+            else if(logic::AreRectsTouching(mouseRect, entityRects.middleEnemyRect)) {attackedAI = 1;}
+            else if(logic::AreRectsTouching(mouseRect, entityRects.bottomEnemyRect)) {attackedAI = 2;}
 
             if(attackedAI != 3) {
                 AIs[attackedAI].health -= 100;
